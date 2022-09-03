4c134f303a768e98b7c8a1491a6ea814427a0a71
Ahtesham <ahtesham2000@gmail.com>
2022-09-03 13:21:16 +0530

    Sep3-database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'root',
+        'password' => '23988725',
+        'database' => 'miniboxoffice',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
